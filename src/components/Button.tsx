7015dde30c01da1116515743122bf5acfaa2d37c
Sebastian Kühne <sebastian.kuehne@outlook.com>
2024-03-30 17:10:24 +0100

    Space increased between icon and label

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -23,7 +23,7 @@ export default function Button({ action, label, icon }: Readonly<ButtonProps>) {
 
   return (
     <button
-      className="mb-2 me-2 flex rounded-lg border border-gray-700 px-5 py-2.5 text-center text-sm font-medium text-gray-700 hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300"
+      className="mb-2 me-2 flex gap-2 rounded-lg border border-gray-700 px-5 py-2.5 text-center text-sm font-medium text-gray-700 hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300"
       onClick={action}
     >
       {!!icon && iconSVG}
