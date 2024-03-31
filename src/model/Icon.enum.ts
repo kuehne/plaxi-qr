be5eec140f97a1ebec8037dec1df08876aabc45b
Sebastian Kühne <sebastian.kuehne@outlook.com>
2024-03-31 15:57:02 +0200

    Added icon source

--- src/model/Icon.enum.ts
+++ src/model/Icon.enum.ts
@@ -1,3 +1,5 @@
+// Icons from https://heroicons.com/
+
 export enum Icon {
   UNDO = "M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3",
   DOWLOAD = "M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3",
